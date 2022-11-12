3ad43f417a82dc436533d16d587dd3270319a91f
swapnilawaridev <117968702+swapnilawaridev@users.noreply.github.com>
2022-11-12 17:36:23 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,11 @@
+bank={'Name':'kotak','ifsc':1223,'branch':'pune','phone':222666}
+print(bank)
+print(type(bank))
+print(bank['Name'])
+print(bank['branch'])
+print(bank)
+bank.pop('phone')
+print(bank)
+bank(get)
+
+
