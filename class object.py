7e29d75029eb9afa79b575fbe3bc9c290b7dca8c
swapnilawaridev <117968702+swapnilawaridev@users.noreply.github.com>
2022-11-13 22:16:39 +0530

    Add files via upload

---
+++ class object.py
@@ -0,0 +1,11 @@
+class Fruits:
+  def __init__(self, name,color, quantity):
+        self.name = name
+        self.color = color
+        self.quantity = quantity
+
+obj= Fruits("Apple","Red",12)
+
+print(obj.name)
+print(obj.color)
+print(obj.quantity)
